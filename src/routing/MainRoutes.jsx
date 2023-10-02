05d0c85846fd1e14b08e1fafebed3513cfa9700c
K1ndoctop <argen.paiysbekov04@gmail.com>
2023-10-02 12:46:48 +0600

    argen

--- src/routing/MainRoutes.jsx
+++ src/routing/MainRoutes.jsx
@@ -1,33 +1,32 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from '../pages/HomePage/HomePage';
-import RegisterPage from '../pages/RegisterPage';
-import LoginPage from '../pages/LoginPage';
+import React from 'react'
+import HomePage from '../pages/HomePage'
+import RegisterPage from '../pages/RegisterPage'
+import LoginPage from '../pages/LoginPage'
+import { Route, Routes } from 'react-router-dom'
 
 const MainRoutes = () => {
-  const ROUTES = [
-    {
-      id: 1,
-      path: '/',
-      element: <HomePage />
-    },
-    {
-      id: 2,
-      path: '/register',
-      element: <RegisterPage />
-    },
-    {
-      id: 3,
-      path: '/login',
-      element: <LoginPage />
-    }
-  ];
-
+    const ROUTES =[
+        {
+            id:1,
+            path:'/',
+            element:<HomePage/>
+        },
+        {
+            id:2,
+            path:'/register',
+            element:<RegisterPage/>
+        },
+        {
+            id:3,
+            path:'/login',
+            element:<LoginPage/>
+        },
+    ]
   return (
     <Routes>
-      {ROUTES.map(route => (
-        <Route key={route.id} path={route.path} element={route.element} />
-      ))}
+        {ROUTES.map(route => (
+            <Route key={route.id} path={route.path} element={route.element}/>
+        ))}
     </Routes>
   )
 }
